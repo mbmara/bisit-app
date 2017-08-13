db45071d1418e0b3fe36fcd2b616c7f564d7bd65
Ramel <tearhear18@gmail.com>
2017-08-13 18:59:03 +0800

    added chkka key

--- app/controllers/api/v1/visitor_controller.rb
+++ app/controllers/api/v1/visitor_controller.rb
@@ -12,7 +12,7 @@ class Api::V1::VisitorController < ApplicationController
 		  f.write image_data
 		end
 
-		client = Chikka::Client.new(client_id:'8797a590b66cd3300345f0ba75834c756c9cffd764a467637886a5cee92ae044', secret_key:'8797a590b66cd3300345f0ba75834c756c9cffd764a467637886a5cee92ae044', shortcode:'292906528')
+		client = Chikka::Client.new(client_id:'8797a590b66cd3300345f0ba75834c756c9cffd764a467637886a5cee92ae044', secret_key:'5e8ebdf79c587e9d547b972b5d99f080a70bf1d0119845f6afab642361dfcc50', shortcode:'292906528')
 		client.send_message(message:'This is a test', mobile_number:'639959801216')
 		
 		json_response true,visitor_params
