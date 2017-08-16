2e0a64ac4d7ebc98fa426a37316c9a0c3ff2d8a5
Ramel <tearhear18@gmail.com>
2017-08-16 23:22:17 +0800

    resolve fucing error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,29 +6,34 @@ class ApplicationController < ActionController::Base
 	end
 
   def authorize_request
-    @header = request.headers['Authorization']
-    if @header == 0
+    begin
+      @header = request.headers['Authorization']
+      if @header == 0
+        json_response false,"Invalid Token"
+        return false
+      end
+      @current_user = User.find(decoded_auth_token[:id]) if decoded_auth_token
+      #verify if token is exist
+      unless @current_user.present?
+        json_response false,"Invalid Token"
+        return false
+      end
+      if @current_user.user_token.nil?
+        json_response false,"Invalid Token"
+        return false
+      end
+      #check token expiration time
+      unless @current_user.user_token.token_created_at >=  30.minutes.ago
+        @current_user.user_token.destroy
+        json_response false,"Invalid Token"
+        return false
+      end
+      @current_user.user_token.update_expiration
+      @current_user.user_token.save
+    rescue
       json_response false,"Invalid Token"
       return false
     end
-    @current_user = User.find(decoded_auth_token[:id]) if decoded_auth_token
-    #verify if token is exist
-    unless @current_user.present?
-      json_response false,"Invalid Token"
-      return false
-    end
-    if @current_user.user_token.nil?
-      json_response false,"Invalid Token"
-      return false
-    end
-    #check token expiration time
-    unless @current_user.user_token.token_created_at >=  30.minutes.ago
-      @current_user.user_token.destroy
-      json_response false,"Invalid Token"
-      return false
-    end
-    @current_user.user_token.update_expiration
-    @current_user.user_token.save
   end
 
   def decoded_auth_token
