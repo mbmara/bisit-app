f08f118992a04297fc9557eedfeea671954bbdbd
Ramel <tearhear18@gmail.com>
2017-10-01 15:57:19 +0800

    working updates

--- app/assets/javascripts/visitor_kiosk/logout/controller.js
+++ app/assets/javascripts/visitor_kiosk/logout/controller.js
@@ -11,6 +11,7 @@
 			var kiosk_logout = this,params={};
 			
 			kiosk_logout.qrcamera_window = true;
+			kiosk_logout.infoWindow = false;
 			kiosk_logout.cancel = cancel;
 			kiosk_logout.abort = abort;
 			kiosk_logout.logout = logout;
@@ -19,26 +20,27 @@
 				params.code = data;
 				kiosk_logout.qrcamera_window = false;
 				visitorFactory.verify(data, function(res){
-					console.log(res);
 					if(res.data.status){
-						kiosk_logout.visitor_info = res.data.payload;	
+						kiosk_logout.visitor_info = res.data.payload;
+						kiosk_logout.infoWindow = true;	
 					}else{
 						kiosk_logout.qrcamera_window = true;
 						Notification.showError(res.data.payload);
 					}
-					
 				})
 			}
 			QRCamera.cancel = function(){
-				kiosk_logout.qrcamera_window = false;
+				kiosk_logout.qrcamera_window = true;
 				$state.go("kiosk");
 			}
 			function cancel(){
+				kiosk_logout.qrcamera_window = true;
 				$state.go("kiosk");
 			}
 
 			function abort(){
 				kiosk_logout.qrcamera_window = true;
+				kiosk_logout.infoWindow = true
 			}
 			function logout(){
 				visitorFactory.logout(params.code, function(res){
@@ -47,6 +49,8 @@
 						delete params.code;
 						kiosk_logout.visitor_info = {};
 						kiosk_logout.qrcamera_window = true;
+						kiosk_logout.infoWindow = true;
+						
 					}else{
 						Notification.showError(res.data.payload)
 					}
